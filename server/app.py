a7d76fcef685a2823cccedce1e6a0b6ad7e916ec
Barbara Tess <barbaratessn@gmail.com>
2024-06-06 11:44:04 +0300

    Assignment complete

--- server/app.py
+++ server/app.py
@@ -17,11 +17,33 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    #Get all plants
+    def get(self):
+        plants = Plant.query.all()
+        plant_dicts = [plant.to_dict() for plant in plants]
+        return make_response(plant_dicts, 200)
+    
+    #Add a new plant
+    def post(self):
+        new_plant_request = request.get_json()
+        new_plant = Plant(name = new_plant_request['name'], image = new_plant_request['image'], price = new_plant_request['price'])
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        new_plant_dict = new_plant.to_dict()
+
+        return make_response(new_plant_dict, 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id = id).first().to_dict()
+
+        return make_response(plant, 200)
         
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
